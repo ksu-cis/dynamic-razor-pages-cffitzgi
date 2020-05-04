6531ab4e5bd63bc31e5433b8d23f656d52f6aa8f
Cashel Fitzgibbons <cffitzgi@ksu.edu>
2020-05-04 18:23:23 -0500

    Done

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -17,54 +17,98 @@ namespace Movies.Pages
         /// <summary>
         /// Search terms for filtering movies
         /// </summary>
-        public string SearchTerms { get; set; } = "";
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerms { get; set; }
 
         /// <summary>
         /// Filtered MPAARatings
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public string[] MPAARatings { get; set; }
         
         /// <summary>
         /// Filtered genres
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public string[] Genres { get; set; }
 
         /// <summary>
         /// Minimum IMDB Rating
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public double? IMDBMin { get; set; }
         /// <summary>
         /// Maximum IMDB Rating
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public double? IMDBMax { get; set; }
 
         /// <summary>
         /// Minimum Rotten Tomatoes
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public int? RTMin { get; set; }
+
         /// <summary>
         /// Maximum rotten tomatoes rating.
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public int? RTMax { get; set; }
 
         /// <summary>
         ///On search.
         /// </summary>
-        public void OnGet(double? imdbMin, double? imdbMax, int? rtMin, int? rtMax)
+        public void OnGet(/*double? imdbMin, double? imdbMax, int? rtMin, int? rtMax*/)
         {
-            SearchTerms = Request.Query["SearchTerms"];
-            MPAARatings = Request.Query["MPAARatings"];
-            Genres = Request.Query["Genres"];
-            IMDBMin = imdbMin;
-            IMDBMax = imdbMax;
-            RTMin = rtMin;
-            RTMax = rtMax;
+            Movies = MovieDatabase.All;
+            if (SearchTerms != null)
+            {
+                Movies = Movies.Where(movie => movie.Title != null && movie.Title.Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase));
+                /*Movies = from movie in Movies
+                         where movie.Title != null && movie.Title.Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase)
+                         select movie;*/
+            }
 
-            Movies = MovieDatabase.Search(SearchTerms);
-            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
-            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
-            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
-            Movies = MovieDatabase.FilterByRottenTomatoesRating(Movies, RTMin, RTMax);
+            if (MPAARatings != null && MPAARatings.Length != 0)
+            {
+                Movies = Movies.Where(movie => movie.MPAARating != null && MPAARatings.Contains(movie.MPAARating));
+            }
+
+            if (Genres != null && Genres.Length != 0)
+            {
+                Movies = Movies.Where(movie => movie.MajorGenre != null && Genres.Contains(movie.MajorGenre));
+            }
+            
+            if (IMDBMin != null)
+            {
+                Movies = Movies.Where(movie => movie.IMDBRating != null && IMDBMin <= movie.IMDBRating);
+            }
+            if (IMDBMax != null)
+            {
+                Movies = Movies.Where(movie => movie.IMDBRating != null && movie.IMDBRating <= IMDBMax);
+            }
+
+            if (RTMin != null)
+            {
+                Movies = Movies.Where(movie => movie.RottenTomatoesRating != null && RTMin <= movie.RottenTomatoesRating);
+            }
+            if (RTMax != null)
+            {
+                Movies = Movies.Where(movie => movie.RottenTomatoesRating != null && movie.RottenTomatoesRating <= RTMax);
+            }
+            //SearchTerms = Request.Query["SearchTerms"];
+            //MPAARatings = Request.Query["MPAARatings"];
+            //Genres = Request.Query["Genres"];
+            //IMDBMin = imdbMin;
+            //IMDBMax = imdbMax;
+            //RTMin = rtMin;
+            //RTMax = rtMax;
+
+            //Movies = MovieDatabase.Search(SearchTerms);
+            //Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            //Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            //Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
+            //Movies = MovieDatabase.FilterByRottenTomatoesRating(Movies, RTMin, RTMax);
         }
     }
 }
